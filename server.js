a9a2cb0b4c92d957e09144459e8afb1ad4224608
drumgiovanni <giovannitheworld@gmail.com>
2018-06-17 16:40:10 +0900

    deploy

--- server.js
+++ server.js
@@ -16,10 +16,9 @@ app.use(express.static(__dirname + '/icon'));
 app.use(express.static(path.join(__dirname, 'client/build')));
 MongoClient.connect(process.env.MONGODB_URI, (err,client) => {
     assert.equal(null, err);
-    console.log("Connected to DB");
     const db = client.db('realDB'); 
     const targetNames = [];
-    
+
     app.get('/api/db', (request, response) => {
         db.collection('alldatas', (error, collection) => {
              collection.find().toArray((error, documents) => {
@@ -36,7 +35,9 @@ MongoClient.connect(process.env.MONGODB_URI, (err,client) => {
         let buf = fs.readFileSync(__dirname +'/icon/parking.png')
         response.send(buf, {'Content-Type': 'image/png'}, 200)
     })
-
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    });
     app.listen(app.get("port"), err => {
         if (err) throw new Error(err)
         else console.log(`listenning on port ${app.get("port")}`);
